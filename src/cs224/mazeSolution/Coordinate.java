a3b90919a641934093d2a4e7daa9381b07b6a0a2
EPICmynamesBG <mynamesbg@gmail.com>
2015-04-13 09:41:39 -0400

    Added heuristic cost estimator.

--- src/cs224/mazeSolution/Coordinate.java
+++ src/cs224/mazeSolution/Coordinate.java
@@ -1,15 +1,39 @@
 package cs224.mazeSolution;
 
+import java.util.ArrayList;
+
 public class Coordinate {
 	
-	private int x;
-	private int y;
+	int x, y, value, heuristicCost;
+    ArrayList<Coordinate> neighbors = new ArrayList<>();
+    boolean visited = false;
+    Coordinate parent = null;
+    boolean inPath = false;
+    double travelled;
+    double projectedDist;
+    boolean wall = true;
+    boolean open = true;
 		
+    Coordinate(int x, int y, int value, boolean isWall) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+        this.heuristicCost = 0;
+        this.wall = isWall;
+    }
+    
+    Coordinate(int x, int y, int value, int heuristicCost, boolean isWall){
+    	this.x = x;
+        this.y = y;
+        this.value = value;
+        this.heuristicCost = heuristicCost;
+        this.wall = isWall;
+    }
+    
 	public Coordinate(int x, int y){
-		this.x = x;
-		this.y = y;
+		this(x, y, 0, 0, true);
 	}
-		
+	
 	public int[] getCoor(){
 		int[] result = {this.x , this.y };
 		return result;
@@ -22,4 +46,50 @@ public class Coordinate {
 	public String getCoorAsString(){
 		return "("+this.x+", "+this.y+")";
 	}
+	
+	public int getCoorValue(){
+		return this.value;
+	}
+	
+	public void setHeuristicCost(int value){
+		this.heuristicCost = value;
+	}
+	
+	public int getHeuristicValue(){
+		return this.heuristicCost;
+	}
+	
+    void addNeighbor(Coordinate other) {
+        if (!this.neighbors.contains(other)) { // avoid duplicates
+            this.neighbors.add(other);
+        }
+        if (!other.neighbors.contains(this)) { // avoid duplicates
+            other.neighbors.add(this);
+        }
+    }
+    
+    boolean isCoordinateBelowNeighbor() {
+        return this.neighbors.contains(new Coordinate(this.x, this.y + 1));
+    }
+    
+    boolean isCoordinateRightNeighbor() {
+        return this.neighbors.contains(new Coordinate(this.x + 1, this.y));
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Coordinate(%s, %s)", x, y);
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Coordinate)) return false;
+        Coordinate otherCoordinate = (Coordinate) other;
+        return (this.x == otherCoordinate.x && this.y == otherCoordinate.y);
+    }
+    
+    @Override
+    public int hashCode() {
+        return this.x + this.y * 256;
+    }
 }
--- src/cs224/mazeSolution/MazeSolver.java
+++ src/cs224/mazeSolution/MazeSolver.java
@@ -1,29 +1,34 @@
 package cs224.mazeSolution;
 
 import java.util.ArrayList;
+import cs224.mazeSolution.Coordinate;
 
 public class MazeSolver {
-	
+	private int dimensionX, dimensionY; // dimension of maze
+	private Coordinate[][] coordinates;
 	public static int[][] uneditedMaze;
 	
 	public static void solve(int[][] maze) {
 		uneditedMaze = maze;
 		Coordinate start = new Coordinate(1,1);
-		Coordinate end = new Coordinate(39, 79);
+		Coordinate end = new Coordinate(79,39);
+		//Coor: maze[x][y]
 		//Start index: maze[1][1]
-		//End index: maze[39][79]
+		//End index: maze[79][39]
 		ArrayList<Coordinate> visitedNodes = new ArrayList<Coordinate>();
 		ArrayList<Coordinate> nodesToVisit = new ArrayList<Coordinate>();
 		ArrayList<Coordinate> navigatedNodes = new ArrayList<Coordinate>();
 		nodesToVisit.add(0, start);
 		
 		int currentScore = 0;
-		int finalScore = currentScore; // + heuristic_cost_estimate(start, goal);
 		
-		Coordinate goal = new Coordinate(39, 79);
+		Coordinate goal = new Coordinate(79,39);
 		
 		while (!nodesToVisit.isEmpty()){
 			Coordinate currentNode = nodesToVisit.get(0);
+			int heuristicCost = heuristic_cost_estimate(currentNode, goal);
+			currentNode.setHeuristicCost(heuristicCost);
+			
 			if (currentNode.equals(goal)){
 //				return reconstructPath(navigatedNodes, goal);
 			}
@@ -42,5 +47,22 @@ public class MazeSolver {
 			totalPath.add(current);
 		}
 		return totalPath;
+	}
+	
+	private static int heuristic_cost_estimate(Coordinate current, Coordinate goal){
+		int manhattanDistanceSum = 0;
+	    for (int x = 0; x < 81; x++) // x-dimension, traversing rows (i)
+	        for (int y = 0; y < 41; y++) { // y-dimension, traversing cols (j)
+	            int value = uneditedMaze[x][y]; 
+	            if (value != 0) { // we don't compute MD for element 0
+	                int targetX = (value - 1) / 81; // expected x-coordinate (row)
+	                int targetY = (value - 1) % 41; // expected y-coordinate (col)
+	                int dx = x - targetX; // x-distance to expected coordinate
+	                int dy = y - targetY; // y-distance to expected coordinate
+	                manhattanDistanceSum += Math.abs(dx) + Math.abs(dy); 
+	            } 
+	        }
+	    int manhattanDistance = manhattanDistanceSum;
+		return manhattanDistance;
 	}
 }
