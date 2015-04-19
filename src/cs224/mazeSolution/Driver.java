198125526772130fb1132e40bad95e3454727e9e
Tara Williams <tewilliams@bsu.edu>
2015-04-18 22:51:43 -0400

    Removed all print lines.

--- src/cs224/mazeSolution/Driver.java
+++ src/cs224/mazeSolution/Driver.java
@@ -1,12 +1,6 @@
 package cs224.mazeSolution;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-
-import cs224.mazeSolution.MazeReader;
-import cs224.mazeSolution.MazeSolver;
 
 public class Driver {
 
@@ -16,10 +10,7 @@ public class Driver {
 		MazeReader reader = new MazeReader();
 		Maze maze = reader.readFile(filePath);
 		MazeSolver solver = new MazeSolver();
-		Maze solvedMaze = solver.solve(maze);
-
-		System.out.println(solvedMaze);
-		
+		Maze solvedMaze = solver.solve(maze);		
 		MazeWriter writer = new MazeWriter();
 		writer.writeFile(solvedMaze);
 	}
